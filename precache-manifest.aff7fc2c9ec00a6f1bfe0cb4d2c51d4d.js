c733384d7542f1e4d2daffca79cf9d1316b74f6b
ndb796 <ndb796@naver.com>
2018-12-30 01:43:57 +0900

    Updates

---
+++ precache-manifest.aff7fc2c9ec00a6f1bfe0cb4d2c51d4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b2a1c277b8a7cbda36e",
+    "url": "/nomad_movie_app/static/css/main.418fc78a.chunk.css"
+  },
+  {
+    "revision": "0b2a1c277b8a7cbda36e",
+    "url": "/nomad_movie_app/static/js/main.0b2a1c27.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/nomad_movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "d46490d6d0cb4dcb83b0",
+    "url": "/nomad_movie_app/static/js/runtime~main.d46490d6.js"
+  },
+  {
+    "revision": "1ad6d26634354b011467dddc5e7598c9",
+    "url": "/nomad_movie_app/index.html"
+  }
+];
